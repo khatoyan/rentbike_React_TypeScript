fa6a58ffa4325a371084c776414b9cb9fb175870
Ilia <balabuev09@gmail.com>
2023-04-01 19:18:40 +0700

    fix svg

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,23 @@
+const Path = require('path');
+
+const sourceDir = Path.resolve(__dirname, '..', 'src');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config, { configType }) => {
+    const candidate = config.module.rules.find((item) => 'icon.svg'.match(item.test));
+    candidate.exclude = [sourceDir];
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 };
